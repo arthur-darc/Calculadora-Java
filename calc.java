ca89e8b1d80a7e49e6b0cff5e78e5dd5b988416c
Arthur7Honorato <arthurhonoratodepaula1@gmail.com>
2025-06-01 18:18:32 -0300

    operacoes

--- calc.java
+++ calc.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class calc {
     static public void main(String[] args){
-        Scanner scan=new Scanner(System.in);
+       try (Scanner scan=new Scanner(System.in)) {
 
         numero n1=new numero();
         numero n2=new numero();
@@ -10,14 +10,40 @@ public class calc {
         String opc="S";
 
         while(opc.equalsIgnoreCase("S" )) {
+            System.out.printf("%nEscolha a operação (+, -, *, /): ");
+            String operacao = scan.next();
             System.out.printf("%nDigite o valor 1: ");
             n1.setValor(scan.nextInt());
             System.out.printf("%nDigite o valor 2: ");
             n2.setValor(scan.nextInt());
-            res.setValor(n1.getValor() + n2.getValor());
-            System.out.printf("%nA soma de %d + %d = %d", n1.getValor(), n2.getValor(), res.getValor());
-            System.out.printf("%nDeseja continuar? (S/N): ");
-            opc=scan.next();
+
+        switch (operacao) {
+                    case "+":
+                        res.setValor(n1.getValor() + n2.getValor());
+                        System.out.printf("%nA soma de %d + %d = %d", n1.getValor(), n2.getValor(), res.getValor());
+                        break;
+                    case "-":
+                        res.setValor(n1.getValor() - n2.getValor());
+                        System.out.printf("%nA subtração de %d - %d = %d", n1.getValor(), n2.getValor(), res.getValor());
+                        break;
+                    case "*":
+                        res.setValor(n1.getValor() * n2.getValor());
+                        System.out.printf("%nA multiplicação de %d * %d = %d", n1.getValor(), n2.getValor(), res.getValor());
+                        break;
+                    case "/":
+                        if (n2.getValor() != 0) {
+                            res.setValor(n1.getValor() / n2.getValor());
+                            System.out.printf("%nA divisão de %d / %d = %d", n1.getValor(), n2.getValor(), res.getValor());
+                        } else {
+                            System.out.println("%nNão é possível dividir por zero!");
+                        }
+                        break;
+                    default:
+                        System.out.println("%nOperação inválida!");
+                    }
+                System.out.printf("%nDeseja continuar? (S/N): ");
+                opc = scan.next();
+            }
         }
     }
 }
